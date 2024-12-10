82dc0104ae0a56c913809f8aec6586304558da5d
R.Anthony <ringoet.anthony@gmail.com>
2024-12-10 23:03:34 +0100

    Nouvelles modifications

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Fonction pour générer des numéros aléatoires uniques
 function generateRandomNumbers(count, max) {
   const numbers = new Set();
   while (numbers.size < count) {
@@ -7,14 +6,11 @@ function generateRandomNumbers(count, max) {
   return Array.from(numbers);
 }
 
-// Fonction pour comparer les numéros de l'utilisateur et les numéros gagnants
 function compareNumbers(userNumbers, lottoNumbers) {
   return userNumbers.filter((num) => lottoNumbers.includes(num));
 }
 
-// Gestionnaire de l'événement "Jouer"
 document.getElementById("playButton").addEventListener("click", () => {
-  // Récupération des numéros de l'utilisateur
   const userNumbers = [
     parseInt(document.getElementById("num1").value),
     parseInt(document.getElementById("num2").value),
@@ -22,19 +18,15 @@ document.getElementById("playButton").addEventListener("click", () => {
     parseInt(document.getElementById("num4").value),
   ];
 
-  // Vérification des entrées utilisateur
   if (userNumbers.some(isNaN)) {
     alert("Veuillez entrer tous les numéros correctement.");
     return;
   }
 
-  // Génération des numéros gagnants
   const lottoNumbers = generateRandomNumbers(4, 9);
 
-  // Comparaison des numéros
   const matches = compareNumbers(userNumbers, lottoNumbers);
 
-  // Affichage des résultats dans le DOM
   document.getElementById(
     "lottoNumbers"
   ).textContent = `Numéros gagnants : ${lottoNumbers.join(", ")}`;
@@ -43,7 +35,6 @@ document.getElementById("playButton").addEventListener("click", () => {
       ? `Vos correspondances : ${matches.sort((a, b) => a - b).join(", ")}`
       : "Vos correspondances : Aucune";
 
-  // Vérification : si tous les chiffres correspondent, afficher l'animation du rectangle
   if (
     matches.length === 4 &&
     matches.every((num) => lottoNumbers.includes(num))
@@ -52,22 +43,18 @@ document.getElementById("playButton").addEventListener("click", () => {
   }
 });
 
-// Affichage et animation du rectangle "WINNER"
 function showWinnerBanner() {
   const banner = document.getElementById("winnerBanner");
 
-  // Affiche avec effet de zoom
   banner.classList.remove("hidden", "hide");
   banner.classList.add("show");
 
-  // Reste affiché pendant 6 secondes, puis disparaît
   setTimeout(() => {
     banner.classList.remove("show");
     banner.classList.add("hide");
-  }, 8000); // 2s pour apparaître + 6s affiché
+  }, 8000);
 }
 
-// Gestion du Gold
 let currentGold = 1000000000;
 
 function updateGoldDisplay() {
